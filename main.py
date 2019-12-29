a2f6af77111f688f8d3a3bff42491d5fe5260490
DavitGalstyan-python <46645271+DavitGalstyan-python@users.noreply.github.com>
2019-12-29 13:14:19 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import os, sys
+from sys import platform
+
+if platform == "linux" or platform == "linux2":
+  if os.getuid() == 0:
+    file_or_dir = input("File or Directory [file, dir]: ")
+    file_name = input("File or directory name for search: ")
+    find_dir = input("Finding directory: ")
+    print()
+    print("*************************************************************")
+    if file_or_dir == "file":
+      os.system("find " + find_dir + " -type f" + " -name " + file_name)
+    elif file_or_dir == "dir":
+      os.system("find " + find_dir + " -type d" + " -name " + file_name)
+    print("*************************************************************")
+    print()
+    print("Process Compileted")
+  else:
+      print("ERROR: application must be run as root")
